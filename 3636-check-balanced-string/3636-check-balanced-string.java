ab12c1114811a4c27252bce9ee8ac69133744ff3
prithika3029 <gcprithi@gmail.com>
2025-07-26 14:52:57 +0530

    Time: 1 ms (97.86%), Space: 42.2 MB (75.74%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.java
+++ 3636-check-balanced-string/3636-check-balanced-string.java
@@ -3,11 +3,12 @@ class Solution {
         int even=0;
         int odd=0;
         for(int i=0;i<num.length();i++){
+            int dig=num.charAt(i)-'0';
             if(i%2==0){
-                even+=num.charAt(i)-'0';
+                even+=dig;
             }
             else{
-                odd+=num.charAt(i)-'0';
+                odd+=dig;
             }
         }
         return even==odd?true:false;
